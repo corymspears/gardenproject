8fdd88e83abffbe6c65d103989f19ba2ef1931e3
Samuel-Abel <samuelsa5760@gmail.com>
2020-03-02 20:16:24 -0500

    another commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { AboutComponent } from './about/about.component';
 import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
 import { AdminComponent } from './admin/admin.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
+  {path:'', redirectTo: 'homepage', pathMatch:'full'},
+  {path:'homepage', component: HomepageComponent},
   {path:"about", component: AboutComponent},
   {path:"product", component: ProductComponent},
   {path:"contact", component: ContactComponent},
